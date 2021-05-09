64dd5581ca73855c429a02f66698d3440455a053
Pablo Rodriguez <prodrigu75@gmail.com>
2021-05-09 16:17:19 -0400

    Selector component added

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CountriesRoutingModule } from './countries-routing.module';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SelectorPageComponent
+  ],
   imports: [
     CommonModule,
     CountriesRoutingModule
